966836f75e74979eb76adae8b4fa4fb5586dffc6
GitDidrik <lindberg.didrik@gmail.com>
2023-04-28 17:25:13 -0700

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP IF EXISTS Company_db;
+CREATE DATABASE Company_db;
+
+USE Company_db;
+
+DROP TABLE IF EXISTS Employee;
+CREATE TABLE Employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (manager_id) REFERENCES Employee(id)
+);
+
+DROP TABLE IF EXISTS Role;
+CREATE TABLE Role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES Department(id)
+);
+
+
+DROP TABLE IF EXISTS Department;
+CREATE TABLE Department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);

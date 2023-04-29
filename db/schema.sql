0dd9a2ae18d866d9cf76d7c04b4df779acc7284a
GitDidrik <lindberg.didrik@gmail.com>
2023-04-28 21:35:00 -0700

    updated schema role to role_id

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE Employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    role VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (manager_id) REFERENCES Employee(id)
